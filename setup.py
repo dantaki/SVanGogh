9fd8557d916e8b54f0d429f64a57d99b90c7047b
dantaki <dantaki@ucsd.edu>
2017-08-06 14:14:36 -0700

    trial fork

---
+++ setup.py
@@ -0,0 +1,20 @@
+try:
+	from setuptools import setup
+except ImportError:
+	from distutils.core import setup
+setup(
+    name='SVanGogh',
+    version='0.1',
+    author='Danny Antaki',
+    author_email='dantaki@ucsd.edu',
+    packages=['svangogh'],
+    scripts=['svangogh/svangogh'],
+    url='https://github.com/dantaki/SVanGogh',
+    license='LICENSE',
+    long_description=open('README').read(),
+    install_requires=[
+        "pysam",
+        "pybedtools",
+	"scipy"
+    ],
+)
